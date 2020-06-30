5f7ccec231162c14f2e8761248b8074eff5ae6b7
Kyle Wong <kylewong2510@gmail.com>
2020-06-30 15:25:14 -0600

    Basic style card container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header'
 import './App.css';
+import MovieCardContainer from './MovieCardContainer';
 
 class App extends Component {
   constructor() {
@@ -14,6 +15,7 @@ class App extends Component {
     return (
       <main>
         <Header currentUser={this.state.currentUser}/>
+        <MovieCardContainer />
       </main>
     )
   }
