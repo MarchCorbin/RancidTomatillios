5f0dae21b63f681863662cc9d0ea09abd53e119f
Corbin March <marchcorbin@gmail.com>
2020-07-01 18:15:44 -0600

    Working on login

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,15 +2,44 @@ import React from 'react'
 import './Login.css'
 
 
-function Login() {
+class Login extends React.Component { 
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.loginCredentials = props.loginCredentials
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({[name]: value})
+  }
+
+  render(){
     return (<form>
-    <input className='username' placeholder='Username'></input>
-    <input className='password' placeholder='Password'></input>
-    <button className='login-button'>Login</button>
-
-
-
-</form>)
+      <input 
+        value={this.state.email}
+        type='text'
+        name='email' 
+        placeholder='Email'
+        onChange={event => this.handleChange(event)}
+      />
+     
+      <input 
+        value={this.state.password}
+        type='text'
+        name='password' 
+        placeholder='Password'
+        onChange={event => this.handleChange(event)}
+      />
+      <button 
+        className='login-button' 
+        onClick={event => this.loginCredentials(event, this.state)}>Login
+      </button>
+      </form>)
+  }
 
 }
 
